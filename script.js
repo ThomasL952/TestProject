f82262d73901f9be07f8fdbdccd9a2033ed74c3c
Thomas Lawrence <evelynlawrence@MacBook-Air.local>
2024-10-27 13:38:27 -0400

    Add button

---
+++ script.js
@@ -0,0 +1,6 @@
+const button = document.querySelector('#my-button');
+const myDiv = document.querySelector('#my-div');
+
+button.addEventListener('click', () => {
+    console.log('Button clicked!');
+  });
